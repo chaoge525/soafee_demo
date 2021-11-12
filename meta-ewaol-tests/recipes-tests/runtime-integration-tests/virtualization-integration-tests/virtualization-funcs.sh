11105c28db83a0744fe59c7aaa2bae66744e2dd7
Richard Neill <richard.neill@arm.com>
2021-11-12 17:07:20 +0000

    ewaol-tests: Add virtualization runtime integration tests
    This patch adds a new BATS- and Expect-based test suite that provides
    integration tests for the ewaol-virtualization DISTRO_FEATURE which implements
    virtualization in EWAOL via the Xen hypervisor.
    
    The integration test suite runs on the Host via the ptest framework, and
    validates that a virtualized Guest is running and has network accessibility, as
    well as that the Host image is able to manage the Guest successfully.
    
    Issue-Id: SCM-3583
    Signed-off-by: Richard Neill <richard.neill@arm.com>
    Change-Id: I935f5a31e178739ff8aad2f285bb4e3f9c313e72

---
+++ meta-ewaol-tests/recipes-tests/runtime-integration-tests/virtualization-integration-tests/virtualization-funcs.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2021, Arm Limited.
+#
+# SPDX-License-Identifier: MIT
+
+get_guest_status() {
+
+    xendomains_output=$(/usr/lib/xen/bin/xendomains status | grep "${1}" \
+                        2>"${TEST_STDERR_FILE}")
+    exitcode="${?}"
+
+    if [ "${exitcode}" != 0 ]; then
+        /usr/lib/xen/bin/xendomains status
+        return "${exitcode}"
+    fi
+
+    guest_status=$(echo "${xendomains_output}" | \
+                   awk '{print $NF}' | \
+                   sed 's/[][]//g' \
+                   2>>"${TEST_STDERR_FILE}")
+
+    echo "${guest_status}"
+    return "${exitcode}"
+
+}
+
+guest_is_running() {
+
+    guest_status=$(get_guest_status "${1}")
+    exitcode="${?}"
+
+    if [ "${exitcode}" != 0 ] && [ "${guest_status}" != "running" ]; then
+        return 1
+    else
+        return 0
+    fi
+
+}
+
+guest_is_not_running() {
+
+    # If the guest is not found then PASS
+    # If the guest is found and it is dead: PASS
+    # If the guest is found and it is not dead: FAIL
+
+    guest_status=$(get_guest_status "${1}")
+    if [ "${?}" -ne 1 ] && [ "${guest_status}" != "dead" ]; then
+        return 1
+    else
+        return 0
+    fi
+
+}
+
+test_guest_login_and_network_access() {
+    expect guest-runtime-validation.expect "${1}" 2>"${TEST_STDERR_FILE}"
+}
+
+shutdown_guest() {
+    systemctl stop xendomains 2>"${TEST_STDERR_FILE}"
+}
+
+start_guest() {
+    systemctl start xendomains 2>"${TEST_STDERR_FILE}"
+}
+
+start_xendomains() {
+    systemctl start xendomains 2>"${TEST_STDERR_FILE}"
+}
+
+xendomains_is_running() {
+    systemctl is-active xendomains 2>"${TEST_STDERR_FILE}"
+}
+
+wait_for_xendomains_to_be_running() {
+
+    status=1
+    _run wait_for_success 300 10 xendomains_is_running
+    if [ "${status}" -ne 0 ]; then
+        echo "Timeout reached before xendomains service is active"
+        return "${status}"
+    fi
+
+    return "${status}"
+}
