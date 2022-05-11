da5751ae734fbb15063556cd405a4a760b1b8c62
Devaraj Ranganna <devaraj.ranganna@arm.com>
2022-05-11 10:42:36 +0000

    ewaol-[config,distro]: Inherit baremetal distro feature
    As part of Cassini and EWAOL alignment, baremetal distro feature
    implementation is moved to Cassini distro and it is the default
    distro feature in Cassini. However, the baremetal distro feature
    in EWAOL is retained, but the implementation is inherited from
    the Cassini distro.
    
    Issue-Id: OSSEMBEDA-1478
    
    Signed-off-by: Devaraj Ranganna <devaraj.ranganna@arm.com>
    Change-Id: I364d202715e924e58d72a2018e5c6c600a7aa804

--- meta-ewaol-distro/recipes-core/base-files/files/ewaol_profile.sh
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2022, Arm Limited.
-#
-# SPDX-License-Identifier: MIT
-
-export PS1='\u@\h:\w\$ '
-
-# Set the same PATH for all users
-PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
